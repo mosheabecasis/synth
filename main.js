f0c33b4016715d5a3418684e0b67acb4ac980807
moshe abecasis <mosh14abou@yahoo.com>
2021-02-15 10:50:25 +0200

    first

---
+++ main.js
@@ -0,0 +1,7 @@
+window.onload
+const $keyBoard = document.getElementById('keyBoard');
+
+    const key = document.createElement('li');
+    key.classList.add('ivory');
+    key.dataset.id = 'c4';
+    $keyBoard.appendChild(key);
