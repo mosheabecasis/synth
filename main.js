9712fdb64ec3d1548371ebda95090a9f84671fd7
moshe abecasis <mosh14abou@yahoo.com>
2021-02-25 10:23:27 +0200

    release

--- main.js
+++ main.js
@@ -2,31 +2,9 @@
 const $keyBoard = document.getElementById('keyboard');
 const $audioTag = document.getElementById('audio');
 const $display = document.getElementById('display');
-//const $container = document.getElementById('container');
-//const $ebonyKeyBoard = document.getElementById('ebonykeyboard');
+
 let keys = ['C4', 'D4', 'E4', 'F4', 'G4', 'A4', 'B4', 'C5', 'D5', 'E5'];
-//let Keys = ['c4', 'd4','e4', 'f4', 'g4', 'a4','b4', 'c5', 'd5','f5','g5','a5'];
-//let keys = ['C4', 'D4b', 'D4', 'E4b', 'E4', 'F4', 'G4b', 'G4', 'A4b', 'A4', 'B4b', 'B4', 'C5', 'D5b', 'D5', 'E5b', 'E5'];
-//let keys = ['c','d','e','f','g','a','b'];
-// const sound = {
-//     C4: './sounds/C4.mp3',
-//     D4b: './sounds/Db4.mp3',
-//     D4: './sounds/D4.mp3',
-//     E4b: './sounds/Eb4.mp3',
-//     E4: './sounds/E4.mp3',
-//     F4: './sounds/F4.mp3',
-//     G4b: './sounds/Gb4.mp3',
-//     G4: './sounds/G4.mp3',
-//     A4b: './sounds/Ab4.mp3',
-//     A4: './sounds/A4.mp3',
-//     B4b: './sounds/Bb4.mp3',
-//     B4: './sounds/B4.mp3',
-//     C5: './sounds/C5.mp3',
-//     D5b: './sounds/Db5.mp3',
-//     D5: './sounds/D5.mp3',
-//     E5b: './sounds/Eb5.mp3',
-//     E5: './sounds/E5.mp3'
-// };
+
 const sound = {
     C4: './sounds/C4.mp3',
     C4sharp: './sounds/Db4.mp3',
@@ -48,18 +26,13 @@ const sound = {
 };
 
 
-// keys.forEach((key) => {
-//     const ivorykey = document.createElement('li');
-//     ivorykey.classList.add('ivory');
-//     ivorykey.dataset.id = key;
-//     $keyBoard.appendChild(ivorykey);
-// });
+
 const play = ($event) => {
     let note = $event.target.dataset.id;
     console.log(note);
     $audioTag.src = sound[note];
     $audioTag.play();
-    $display.innerText=note;
+    $display.innerText=note.replace('sharp','#');
     
 };
 
@@ -82,21 +55,7 @@ keys.forEach((key) => {
     }
 });
 
-// keys.forEach((key) => {
-//     if (!key.includes('b')) {
-//         const ivorykey = document.createElement('li');
-//         ivorykey.classList.add('ivory');
-//         ivorykey.dataset.id = key;
-//         $keyBoard.appendChild(ivorykey);
 
-//     } else {
-//         const ebonykey = document.createElement('li');
-//         ebonykey.classList.add('ebony');
-
-//         ebonykey.dataset.id = key;
-//         $keyBoard.appendChild(ebonykey);
-//     }
-// });
 
 $keyBoard.addEventListener('click', play);
 
